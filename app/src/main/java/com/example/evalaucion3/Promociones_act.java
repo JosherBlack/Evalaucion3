7ea20b77baf62e3d0e3fe9abeb3e227ceef503d1
Josher <josher.f@gmail.com>
2020-11-25 12:59:10 -0300

    Creación de la interfaz gráfica

--- app/src/main/java/com/example/evalaucion3/Promociones_act.java
+++ app/src/main/java/com/example/evalaucion3/Promociones_act.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Promociones_act extends AppCompatActivity {
+public class Promociones_act extends AppCompatActivity
+{
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promociones_act);
     }
